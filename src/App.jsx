a13330b9106878fb40139a258bf780c055fc487d
Prasoon9336 <prasoonsrivastava44@gmail.com>
2026-06-26 21:00:18 +0530

    master

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Navigation from "./components/Navigation";
-import BackgroundVideo from "./components/BackgroundVideo";
+import Lightfall from "./components/Lightfall";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ProjectsPage from "./pages/ProjectsPage";
@@ -21,8 +21,24 @@ export default function App() {
 
   return (
     <>
-      <div className="video-bg-fixed">
-        <BackgroundVideo />
+      <div className="lightfall-bg-fixed">
+        <Lightfall
+          colors={["#A6C8FF", "#5227FF", "#FF9FFC"]}
+          backgroundColor="#0A29FF"
+          speed={0.5}
+          streakCount={4}
+          streakWidth={1}
+          streakLength={1}
+          glow={1}
+          density={0.5}
+          twinkle={0.9}
+          zoom={3}
+          backgroundGlow={0.5}
+          opacity={1}
+          mouseInteraction
+          mouseStrength={0.5}
+          mouseRadius={0.1}
+        />
       </div>
       <Navigation currentPage={page} onNavigate={setPage} />
       <div className="scrollable-content">
--- src/components/BackgroundVideo.jsx
+++ src/components/BackgroundVideo.jsx
@@ -10,7 +10,7 @@ export default function VideoBackground() {
         playsInline 
         className="bg-video"
       >
-        <source src="https://res.cloudinary.com/dp2mxstew/video/upload/v1781962718/samples/sea-turtle.mp4" type="video/mp4" />
+        <source src="https://res.cloudinary.com/dp2mxstew/video/upload/v1782481989/lightfall-1782415737345_lt87u0.mkv" type="video/mp4" />
       </video>
     </div>
   );
