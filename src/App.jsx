f59d8bb98497f7ce9089c704d428759e4a09542f
Prasoon9336 <prasoonsrivastava44@gmail.com>
2026-09-03 00:13:34 +0530

    New LiquidEther Background Update!

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Navigation from "./components/Navigation";
-import Lightfall from "./components/Lightfall";
+// import Lightfall from "./components/Lightfall";
+import LiquidEther from './components/LiquidEther';
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ProjectsPage from "./pages/ProjectsPage";
@@ -23,7 +24,7 @@ export default function App() {
 
   return (
     <>
-      <div className="lightfall-bg-fixed">
+      {/* <div className="lightfall-bg-fixed">
         <Lightfall
           colors={["#A6C8FF", "#5227FF", "#FF9FFC"]}
           backgroundColor="#0A29FF"
@@ -41,7 +42,29 @@ export default function App() {
           mouseStrength={0.5}
           mouseRadius={0.04}
         />
-      </div>
+      </div> */}
+      <div style={{ width: '100%', height: 600, position: 'relative' }}>
+  <LiquidEther
+    colors={[ '#5227FF', '#FF9FFC', '#B497CF' ]}
+    mouseForce={20}
+    cursorSize={70}
+    isViscous
+    viscous={30}
+    iterationsViscous={32}
+    iterationsPoisson={32}
+    resolution={0.5}
+    isBounce={true}
+    autoDemo
+    autoSpeed={0.5}
+    autoIntensity={2.2}
+    takeoverDuration={0.25}
+    autoResumeDelay={3000}
+    autoRampDuration={0.6}
+    color0="#5227FF"
+    color1="#FF9FFC"
+    color2="#B497CF"
+/>
+</div>
       <Navigation currentPage={page} onNavigate={setPage} />
       <div className="scrollable-content">
         <div key={page} className="page-content">
